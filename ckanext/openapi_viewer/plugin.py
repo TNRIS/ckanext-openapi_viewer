ae8e2f7a3629bb37e7627e40b4660d717724a036
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2023-04-06 18:04:16 +0200

    OpenAPI Viewer assets and template added

--- ckanext/openapi_viewer/plugin.py
+++ ckanext/openapi_viewer/plugin.py
@@ -4,6 +4,8 @@ import ckan.plugins.toolkit as toolkit
 
 class OpenapiViewerPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IConfigurable, inherit=True)
+    plugins.implements(plugins.IResourceView, inherit=True)
 
     # IConfigurer
 
@@ -12,3 +14,32 @@ class OpenapiViewerPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic',
             'openapi_viewer')
+        
+    
+    def info(self):
+        return {
+            'name': 'openapi_viewer',
+            'title': 'OpenAPI Viewer',
+            'icon': 'file',
+            'default_title': 'OpenAPI Viewer',
+            'filterable': False,
+            'iframed': False,
+            'always_available': True,
+            'schema': {
+                'fields': [
+                    {
+                        'type': 'text',
+                        'id': 'url',
+                        'label': 'URL',
+                        'validators': ['not_empty']
+                    }
+                ]
+            }
+        }
+
+    def can_view(self, data_dict):
+        breakpoint()
+        return data_dict['resource'].get('format', '').lower() == 'json'
+    
+    def view_template(self, context, data_dict):
+        return 'openapi_viewer.html'
